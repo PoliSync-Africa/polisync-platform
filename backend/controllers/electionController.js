a4038759aa56d4821d2123b9346ec1dee366bcc2
md9j84dptw-cloud <md9j84dptw@privaterelay.appleid.com>
2026-08-18 13:12:51 +0000

    Create electionController.js

---
+++ backend/controllers/electionController.js
@@ -0,0 +1,35 @@
+const Election = require("../models/Election");
+
+// Create Election
+exports.createElection = async (req, res) => {
+  try {
+    const election = await Election.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      election
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+};
+
+// Get Elections
+exports.getElections = async (req, res) => {
+  try {
+    const elections = await Election.find().sort({ createdAt: -1 });
+
+    res.json({
+      success: true,
+      elections
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+};
