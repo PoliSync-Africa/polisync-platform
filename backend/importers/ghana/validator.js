c73c38f3efc036562d2335dde79b0d0ba4cecd28
md9j84dptw-cloud <md9j84dptw@privaterelay.appleid.com>
2026-08-18 20:25:22 +0000

    Create validator.js

---
+++ backend/importers/ghana/validator.js
@@ -0,0 +1,14 @@
+function validatePollingStation(row) {
+
+  return (
+    row.region &&
+    row.constituency &&
+    row.code &&
+    row.name
+  );
+
+}
+
+module.exports = {
+  validatePollingStation
+};
