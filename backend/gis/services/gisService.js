413c677449ccc42aa177d59cd1fbc5944f9c956a
md9j84dptw-cloud <md9j84dptw@privaterelay.appleid.com>
2026-08-18 19:50:57 +0000

    Create gisService.js

---
+++ backend/gis/services/gisService.js
@@ -0,0 +1,18 @@
+const { loadGeoJSON } = require("../loaders/geoJsonLoader");
+
+function getGhanaRegions() {
+  return loadGeoJSON("ghana/regions/ghana-regions.geojson");
+}
+
+function getRegionBoundary(regionCode) {
+  const data = getGhanaRegions();
+
+  return data.features.find(
+    feature => feature.properties.code === regionCode
+  );
+}
+
+module.exports = {
+  getGhanaRegions,
+  getRegionBoundary
+};
