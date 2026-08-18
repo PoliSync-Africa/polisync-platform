1a5f108698786062c1e48abd0f4c53465700cd88
md9j84dptw-cloud <md9j84dptw@privaterelay.appleid.com>
2026-08-18 13:04:33 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,19 +5,17 @@ const app = express();
 
 const indexRoutes = require("./routes/index");
 const authRoutes = require("./routes/auth");
+const resultsRoutes = require("./routes/results");
 
 const PORT = process.env.PORT || 5000;
 
-// Connect Database
 connectDB();
 
 app.use(express.json());
 
-// Main API
 app.use("/", indexRoutes);
-
-// Authentication
 app.use("/auth", authRoutes);
+app.use("/results", resultsRoutes);
 
 app.listen(PORT, () => {
   console.log(`POLISYNC server running on port ${PORT}`);
