736a27c46d0d2b7899ea476c21ba3b9bc5f165e5
md9j84dptw-cloud <md9j84dptw@privaterelay.appleid.com>
2026-08-18 08:13:17 +0000

    Create page.tsx

---
+++ frontend/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>POLISYNC AFRICA</h1>
+      <p>Secure Political Intelligence Platform</p>
+
+      <button>Login</button>
+      <button>Create Account</button>
+    </main>
+  );
+}
