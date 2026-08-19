2362a605123c42b1400fa70e11da6bf3faff59d6
md9j84dptw-cloud <md9j84dptw@privaterelay.appleid.com>
2026-08-19 13:29:20 +0000

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
+const authRoutes = require("./routes/auth");
 const healthRoutes = require("./routes/health");
 
 const app = express();
@@ -16,6 +17,7 @@ app.get("/", (req, res) => {
   });
 });
 
+app.use("/auth", authRoutes);
 app.use("/health", healthRoutes);
 
 module.exports = app;
