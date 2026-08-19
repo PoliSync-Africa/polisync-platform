ce79fc321f73e689be3dce4b0f6b05366e6a5a7a
md9j84dptw-cloud <md9j84dptw@privaterelay.appleid.com>
2026-08-19 21:26:21 +0000

    Connect polling station routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,7 @@ const resultsRoutes = require("./routes/results");
 const electionsRoutes = require("./routes/elections");
 const organizationRoutes = require("./routes/organizations");
 const organizationUnitRoutes = require("./routes/organizationUnits");
+const pollingStationRoutes = require("./routes/pollingStations");
 const adminRoutes = require("./routes/admin");
 
 const app = express();
@@ -14,6 +15,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Root API
 app.get("/", (req, res) => {
   res.json({
     name: "POLISYNC AFRICA API",
@@ -22,12 +24,14 @@ app.get("/", (req, res) => {
   });
 });
 
+// API Routes
 app.use("/auth", authRoutes);
 app.use("/health", healthRoutes);
 app.use("/results", resultsRoutes);
 app.use("/elections", electionsRoutes);
 app.use("/organizations", organizationRoutes);
 app.use("/organization-units", organizationUnitRoutes);
+app.use("/polling-stations", pollingStationRoutes);
 app.use("/admin", adminRoutes);
 
 module.exports = app;
