c2b2107b6cfd473b1d4739d901053c180b40b4e1
md9j84dptw-cloud <md9j84dptw@privaterelay.appleid.com>
2026-08-19 13:23:03 +0000

    Update auth.js

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,1 +1,19 @@
+const express = require("express");
+const router = express.Router();
 
+const authController = require("../controllers/authController");
+
+// Status
+router.get("/", (req, res) => {
+  res.json({
+    status: "success",
+    message: "Authentication routes ready"
+  });
+});
+
+// Authentication Routes
+router.post("/register", authController.register);
+router.post("/login", authController.login);
+router.post("/forgot-password", authController.forgotPassword);
+
+module.exports = router;
