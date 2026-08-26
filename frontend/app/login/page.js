29d70b7e201654c8229166810ffafbf1ab0424ae
md9j84dptw-cloud <md9j84dptw@privaterelay.appleid.com>
2026-08-26 22:25:46 +0000

    Update page.js

--- frontend/app/login/page.js
+++ frontend/app/login/page.js
@@ -788,7 +788,7 @@ export default function LoginPage() {
                 "16px",
 
               borderRadius:
-                "999px",
+  "12px",
 
               border:
                 "none",
