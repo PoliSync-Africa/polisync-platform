3745920bddba8be2c50a4f019a48380475cbc726
md9j84dptw-cloud <md9j84dptw@privaterelay.appleid.com>
2026-08-27 23:29:30 +0000

    Add Ghana regions GIS controller endpoints

--- backend/controllers/gisController.js
+++ backend/controllers/gisController.js
@@ -1,22 +1,17 @@
 const gisService = require("../gis/services/gisService");
 
 exports.getGhanaRegions = (req, res) => {
-
   res.json({
     success: true,
-    data: gisService.getGhanaRegions()
+    data: gisService.getGhanaRegions(),
   });
-
 };
 
 exports.getRegion = (req, res) => {
-
-  const region =
-    gisService.getRegionBoundary(req.params.code);
+  const region = gisService.getRegionBoundary(req.params.code);
 
   res.json({
     success: true,
-    data: region
+    data: region,
   });
-
 };
