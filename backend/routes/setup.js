b94682216a84c7b8bc4b9e12c21105047c04e26d
md9j84dptw-cloud <md9j84dptw@privaterelay.appleid.com>
2026-08-30 08:19:17 +0000

    Delete backend/routes/setup.js

--- backend/routes/setup.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { bootstrapSuperAdmin } = require("../controllers/setupController");
-
-router.get("/bootstrap-super-admin", bootstrapSuperAdmin);
-
-module.exports = router;
