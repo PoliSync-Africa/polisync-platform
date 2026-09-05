e94af955773efbfe349a610b4a13f6a1c4ac0f4e
md9j84dptw-cloud <md9j84dptw@privaterelay.appleid.com>
2026-09-05 07:28:47 -0700

    Remove visible Home button from dashboard

--- frontend/app/dashboard/page.js
+++ frontend/app/dashboard/page.js
@@ -1,4 +1,3 @@
 "use client";
 import UniversalUserDashboard from "../../components/dashboard/UniversalUserDashboard";
-import HomeNavigationButton from "../../components/dashboard/HomeNavigationButton";
-export default function Dashboard(){return <><HomeNavigationButton/><UniversalUserDashboard/></>;}
+export default function Dashboard(){return <UniversalUserDashboard/>;}
