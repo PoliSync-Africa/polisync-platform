234c8903741ebdaffb357a900d2d8908a15fe968
md9j84dptw-cloud <md9j84dptw@privaterelay.appleid.com>
2026-09-08 07:35:40 -0700

    Auto-sync election geography before election views

--- backend/routes/elections.js
+++ backend/routes/elections.js
@@ -2,15 +2,25 @@ const express = require("express");
 const router = express.Router();
 const electionController = require("../controllers/electionController");
 const { protect } = require("../middleware/auth");
+const { synchronizeAllElectionGeography } = require("../services/electionGeographySyncService");
+
+const syncGeography = async (req, res, next) => {
+  try {
+    await synchronizeAllElectionGeography();
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
 
 router.get("/access", protect, electionController.getElectionAccess);
 router.get("/parties", protect, electionController.getPoliticalParties);
 router.post("/parties", protect, electionController.createPoliticalParty);
 router.patch("/parties/:partyId/logo", protect, electionController.updatePoliticalPartyLogo);
-router.get("/", protect, electionController.getElections);
-router.get("/live", protect, electionController.getLiveElections);
-router.get("/history", protect, electionController.getElectionHistory);
-router.get("/:id", protect, electionController.getElection);
+router.get("/", protect, syncGeography, electionController.getElections);
+router.get("/live", protect, syncGeography, electionController.getLiveElections);
+router.get("/history", protect, syncGeography, electionController.getElectionHistory);
+router.get("/:id", protect, syncGeography, electionController.getElection);
 router.post("/create", protect, electionController.createElection);
 router.patch("/:id", protect, electionController.updateElection);
 router.delete("/:id", protect, electionController.deleteElection);
